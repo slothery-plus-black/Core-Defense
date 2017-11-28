7831caa51f4813ce4bf0b9d70d48c5c26af147cd
Jaime Calvo <jaime230593@hotmail.es>
2017-11-28 13:53:16 +0100

    tiles con animacion destruccion

--- src/js/casilla.js
+++ src/js/casilla.js
@@ -8,6 +8,7 @@ function casilla(nombre,x,y,image,mov,destructible,srcImagenes) {
 	this.destructible = destructible;
 	this.golpes = 0;
 	this.srcImagenes = srcImagenes;
+	this.destruido = false;
 	
 	this.setImagen = function(image, mov){
 		this.image = image;
@@ -15,22 +16,37 @@ function casilla(nombre,x,y,image,mov,destructible,srcImagenes) {
 	}
 
 	this.golpear = function(){
-		if (this.destructible){
+		if (this.destructible && !this.destruido){
 			this.golpes++;
 
 			if (this.golpes === 3){
+				this.destruido = true;
 				this.destruir();
 			}else{
 				var img = new Image();
-				img.src = this.srcImagenes+""+this.nombre+"_"+this.golpes+".png";
+				img.src = this.srcImagenes+""+this.nombre+"-"+this.golpes+".png";
 				this.setImagen(img,false);
 			}
 		}
 	}
 
 	this.destruir = function(){
-		var img = new Image();
-		img.src = this.srcImagenes+"0.png";
-		this.setImagen(img,true);
+		
+		var _this = this;
+
+		//Se va a llamar a si misma para ir avanzando la animacion de destruccion del tile
+		setTimeout(function(){
+			if (_this.golpes === 6){
+				var img = new Image();
+				img.src = _this.srcImagenes+""+_this.nombre+"-"+_this.golpes+".png";
+				_this.setImagen(img,true);
+			}else{
+				var img = new Image();
+				img.src = _this.srcImagenes+""+_this.nombre+"-"+_this.golpes+".png";
+				_this.setImagen(img,false);
+				_this.golpes++;
+				_this.destruir();
+			}
+		}, 50);
 	}
 }
