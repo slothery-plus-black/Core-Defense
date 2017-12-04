b166ce1c32050bd42a2e12838cc5ec0dd9761cef
Jesus Brey <jesusbrey@gmail.com>
2017-12-04 16:58:35 +0100

    Pagina Principal actualizada

---
+++ src/js/scenemanager.js
@@ -0,0 +1,19 @@
+function SceneManager(){
+	var scenes = [];
+    var numScenes=1;
+    this.init = function(){
+        //scenes[0]= new nombreEscena1();
+        //scenes[1]= new nombreEscena2();
+        //etc
+    }
+    var clearscene = function(){
+        var canvas= document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0,  canvas.width, canvas.height);
+    }
+    
+    this.startScene(num){
+        clearscene();
+        scene[num].init();
+    }
+}
